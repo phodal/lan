7f2cdff039d265887a83d5391a6ee1a8028dbd7e
Fengda HUANG <h@phodal.com>
2015-07-29 23:58:16 +0800

    update get/post/put method

--- modules/http_api.js
+++ modules/http_api.js
@@ -1,9 +1,26 @@
 module.exports = function (app) {
-	app.get('/topics', function (req, res) {
-		res.send("----------------");
+	app.get(/^\/topics\/(.+)$/, function (req, res) {
+		var topic = req.params[0];
+		//数据库操作
+		res.json({'topic': topic});
 	});
 
-	return app.put('/topics', function (req, res) {
-		return res.send(204);
+	function update(req, res) {
+		var topic = req.params[0];
+		var payload;
+		if (req.is("json")) {
+			payload = req.body;
+		} else {
+			payload = req.body.payload;
+		}
+		//数据库操作
+		return res.sendStatus(204);
+	}
+
+	app.post(/^\/topics\/(.+)$/, function (req, res) {
+		return update(req, res);
 	});
+
+	return app.put(/^\/topics\/(.+)$/, function (req, res) {
+		update(req, res);	});
 };
