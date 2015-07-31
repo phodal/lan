aae95a43272afdfb0174944927c7d69e339de7a4
Fengda HUANG <h@phodal.com>
2015-07-31 08:54:29 +0800

    coverage test

--- test/server/app_spec.js
+++ test/server/app_spec.js
@@ -1,5 +1,6 @@
 var helper = require('../spec_helper');
 var expect = require('chai').expect;
+var sinon = require('sinon');
 var mqtt = require('mqtt');
 var Model = require('../../models/basic');
 
@@ -13,9 +14,12 @@ describe('Application', function () {
 	it('should able connect to mqtt server', function (done) {
 		var client = mqtt.connect('mqtt://127.0.0.1');
 		var model = new Model();
+		sinon.spy(model, "findOrCreate");
+
 		client.on('connect', function(){
 			client.publish('hello', 'coap');
 			client.end();
+			expect(model.findOrCreate.calledOnce);
 			done();
 		});
 	});
