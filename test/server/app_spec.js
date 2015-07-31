6db8db86f74d5e028d5dd2d0c093e7ff669ffd8a
Fengda HUANG <h@phodal.com>
2015-07-31 11:45:45 +0800

    create test suite

--- test/server/app_spec.js
+++ test/server/app_spec.js
@@ -13,17 +13,20 @@ describe('Application', function () {
 		helper.globalSetup();
 	});
 
-	it('should able connect to mqtt server', function (done) {
-		var client = mqtt.connect('mqtt://127.0.0.1');
-		var model = new Model();
-		sinon.spy(model, "findOrCreate");
+	describe("MQTT Server", function () {
+		it('should able connect to mqtt server', function (done) {
+			var client = mqtt.connect('mqtt://127.0.0.1');
+			var model = new Model();
+			sinon.spy(model, "findOrCreate");
 
-		client.on('connect', function () {
-			client.publish('hello', 'coap');
-			client.end();
-			expect(model.findOrCreate.calledOnce);
-			done();
+			client.on('connect', function () {
+				client.publish('hello', 'coap');
+				client.end();
+				expect(model.findOrCreate.calledOnce);
+				done();
+			});
 		});
+
 	});
 
 	describe("HTTP Server", function () {
@@ -43,17 +46,19 @@ describe('Application', function () {
 		});
 	});
 
-	it('should able connect to coap server', function (done) {
-		var req = coap.request('coap://localhost/hello');
-		var result = {"method":"get"};
+	describe("CoAP Server", function () {
+		it('should able connect to coap server', function (done) {
+			var req = coap.request('coap://localhost/hello');
+			var result = {"method":"get"};
 
-		req.on('response', function(res) {
-			var response_result = JSON.parse(res.payload.toString());
-			if(response_result.method === result.method){
-				done();
-			}
+			req.on('response', function(res) {
+				var response_result = JSON.parse(res.payload.toString());
+				if(response_result.method === result.method){
+					done();
+				}
+			});
+
+			req.end();
 		});
-
-		req.end();
 	});
 });
