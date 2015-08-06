0bff9aa589d76116c0978052b74df292a850b406
Fengda HUANG <h@phodal.com>
2015-08-06 22:38:03 +0800

    add coap test scripts

---
+++ test_scripts/coap_test.js
@@ -0,0 +1,22 @@
+var coap     = require('coap');
+var request  = coap.request;
+var bl       = require('bl');
+var req = request({hostname: 'localhost',port:5683,pathname: '',method: 'POST'});
+
+var payload = {
+  title: 'this is a test payload',
+  body: 'containing nothing useful'
+}
+
+req.setHeader("Accept", "application/json");
+req.setOption('Block2',  [new Buffer('phodal'), new Buffer('phodal')]);
+req.write(JSON.stringify(payload));
+req.on('response', function(res) {
+  res.pipe(bl(function(err, data) {
+    console.log(data);
+    process.exit(0);
+  }));
+
+});
+
+req.end();
