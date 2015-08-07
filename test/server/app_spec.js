e80676a7551efb84f5cef51c6e5a2e47c7afc660
Fengda HUANG <h@phodal.com>
2015-08-07 08:49:33 +0800

    add test for coap put with auth

--- test/server/app_spec.js
+++ test/server/app_spec.js
@@ -109,5 +109,28 @@ describe('Application', function () {
 
 			req.end();
 		});
+
+    it('should abe to put data with auth', function (done) {
+      var request  = coap.request;
+      var bl       = require('bl');
+      var req = request({hostname: 'localhost',port:5683,pathname: '',method: 'POST'});
+
+      var payload = {
+        title: 'this is a test payload',
+        body: 'containing nothing useful'
+      };
+
+      req.setHeader("Accept", "application/json");
+      req.setOption('Block2',  [new Buffer('phodal'), new Buffer('phodal')]);
+      req.write(JSON.stringify(payload));
+      req.on('response', function(res) {
+        if(res.code === '2.06') {
+          done();
+        }
+      });
+
+      req.end();
+
+    });
 	});
 });
