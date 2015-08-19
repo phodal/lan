ad0395a8fbb5a5b898dda1cf759ce5f2cc61cc03
Fengda HUANG <h@phodal.com>
2015-08-19 08:51:20 +0800

    configurable mongodb

--- persistence/mongo.js
+++ persistence/mongo.js
@@ -1,8 +1,10 @@
 var mongo = require('mongodb');
 var MongoClient = mongo.MongoClient;
 
-var dbName = 'myproject';
-var url = 'mongodb://localhost:27017/' + dbName;
+var config = require('config');
+
+//var url = 'mongodb://localhost:27017/myproject';
+var url = config.get('db_url');
 
 function MongoPersistence () {
 
@@ -11,7 +13,7 @@ function MongoPersistence () {
 MongoPersistence.prototype.insert = function(payload) {
   MongoClient.connect(url, function(err, db) {
     var insertDocuments = function(db, callback) {
-      var collection = db.collection('documents');
+      var collection = db.collection(config.get('db_collection'));
       collection.insert(payload, function(err, result) {
         callback(result);
       });
@@ -25,7 +27,7 @@ MongoPersistence.prototype.insert = function(payload) {
 MongoPersistence.prototype.query = function(queryOptions, queryCB) {
   MongoClient.connect(url, function(err, db) {
     var findDocuments = function(db, query, callback) {
-      var collection = db.collection('documents');
+      var collection = db.collection(config.get('db_collection'));
       collection.find(query, {'data': true, '_id': false}).toArray(function(err, docs) {
         callback(docs);
       });
