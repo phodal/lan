84c053ab77a80576a01d9f975d964277eebbfbb6
Fengda HUANG <h@phodal.com>
2015-08-19 16:33:17 +0800

    update mqtt auth

--- modules/mqtt.js
+++ modules/mqtt.js
@@ -4,67 +4,62 @@ var isJson = require('../utils/common').isJson;
 var model = require('../models');
 
 module.exports = function (app) {
-	return function (client) {
+  return function (client) {
     var auth = {};
-    var unsubscribeAll = function() {
+    var userInfo = {};
+    var unsubscribeAll = function () {
 
     };
 
-		client.on('connect', function (packet) {
-      if(packet.username === undefined || packet.password === undefined) {
-        return client.stream.end();
+    client.on('connect', function (packet) {
+      if (packet.username === undefined || packet.password === undefined) {
+        return client.connack({
+          returnCode: -1
+        });
       }
       auth['name'] = packet.username;
       auth['password'] = packet.password.toString();
-			client.id = packet.client;
-			return client.connack({
-				returnCode: 0
-			});
-		});
-		client.on('subscribe', function (packet) {
-      if(packet.username === undefined || packet.password === undefined) {
-        return client.unsuback({
-          messageId: packet.messageId
+      client.id = packet.client;
+      model.User.findOne({where: {name: auth.name}}).then(function (user) {
+        if (!user) {
+          return client.connack({
+            returnCode: -1
+          });
+        }
+        userInfo = user;
+        user.comparePassword(auth.password, function (err, result) {
+          return client.connack({
+            returnCode: 0
+          });
         });
-      }
-			db.subscribe(packet.username, function(result){
+      });
+    });
+    client.on('subscribe', function (packet) {
+      console.log('subscribe');
+      db.subscribe(packet.username, function (result) {
         return result;
       });
-		});
-		client.on('publish', function (packet) {
-      model.User.findOne({where: {name: auth.name}}).then(function (user) {
-        if(!user){
-          return {};
-        }
-        user.comparePassword(auth.password, function(err, result){
-          if(result) {
-            var payload = {'name': user.name, 'token': user.uid,'data': packet.payload.toString()};
-            db.insert(payload);
-          } else {
-            return {
-              'status': 'failure',
-              'error': 'password or username error'
-            };
-          }
-        });
-      });
-		});
-		client.on('pingreq', function (packet) {
+    });
+    client.on('publish', function (packet) {
+      var payload = {'name': userInfo.name, 'token': userInfo.uid, 'data': packet.payload.toString()};
+      db.insert(payload);
+    });
+    client.on('pingreq', function (packet) {
       return client.pingresp();
-		});
-		client.on('disconnect', function () {
+    });
+    client.on('disconnect', function () {
       return client.stream.end();
-		});
-		client.on('error', function (error) {
+    });
+    client.on('error', function (error) {
       return client.stream.end();
-		});
-		client.on('close', function (err) {
+    });
+    client.on('close', function (err) {
       return unsubscribeAll();
-		});
-		return client.on('unsubscribe', function (packet) {
+    });
+    return client.on('unsubscribe', function (packet) {
       return client.unsuback({
         messageId: packet.messageId
       });
-		});
-	};
+    });
+  };
 };
