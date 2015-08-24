8eb98ef69b89fc81413d752f0f6b61f50517c3d4
Fengda HUANG <h@phodal.com>
2015-08-24 08:57:40 +0800

    make port configurable

--- server.js
+++ server.js
@@ -3,29 +3,30 @@ var _ = require('underscore');
 var mqtt = require("mqtt");
 var coap = require("coap");
 var WebSocketServer = require('ws').Server;
+var config = require('config');
 
 start = function (opts, callback) {
   var app = configure();
-  app.listen(8899, function () {
-    console.log("http server run on http://localhost:8899");
+  app.listen(config.get('port.http'), function () {
+    console.log("http server run on port %d", config.get('port.http'));
   });
 
 
   if (_.include(app.config.get('modules'), 'websocket')) {
-    var webSocketServer = new WebSocketServer({port: 8898});
+    var webSocketServer = new WebSocketServer({port: config.get('port.websocket')});
     app.websocket(webSocketServer);
-    console.log("websocket server listening on port %d", 8898);
+    console.log("websocket server listening on port %d", config.get('port.websocket'));
   }
 
   if (_.include(app.config.get('modules'), 'coap')) {
-    coap.createServer(app.coap).listen(5683, function () {
-      console.log("coap server listening on port %d", 5683);
+    coap.createServer(app.coap).listen(config.get('port.coap'), function () {
+      console.log("coap server listening on port %d", config.get('port.coap'));
     });
   }
 
   if (_.include(app.config.get('modules'), 'mqtt')) {
-    mqtt.createServer(app.mqtt).listen(1883, function () {
-      console.log("mqtt server listening on port %d", 1883);
+    mqtt.createServer(app.mqtt).listen(config.get('port.mqtt'), function () {
+      console.log("mqtt server listening on port %d", config.get('port.mqtt'));
     });
   }
   return app;
