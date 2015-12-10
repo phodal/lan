23bc59ca66e85784bea1d60a1ae97a45d9eef557
Fengda HUANG <h@phodal.com>
2015-12-11 07:38:51 +0800

    Test for mqtt publish

--- test/server/mqtt_spec.js
+++ test/server/mqtt_spec.js
@@ -1,7 +1,5 @@
 var mqtt = require('mqtt');
 var request = require('request');
-var coap = require('coap');
-var assert = require('chai').assert;
 var should = require('should');
 var supertest = require('supertest');
 var env = require("../../app.js");
@@ -10,27 +8,63 @@ describe('MQTT Services Test', function () {
   var app, server, mqttServer;
   app = env.configure();
 
-  before(function () {
+  beforeEach(function () {
     mqttServer = mqtt.MqttServer(app.mqtt).listen(1883, function () {
     });
   });
 
-  after(function () {
+  afterEach(function () {
     mqttServer.close();
   });
 
-  it('should able connect to mqtt server', function (done) {
+  describe('connect test', function () {
+    it('should able connect to mqtt server', function (done) {
+      var client = mqtt.connect('http://127.0.0.1', {
+        username: 'root',
+        password: 'root'
+      });
+
+      client.on('connect', function (packet) {
+        if(packet.returnCode === 0){
+          client.end();
+          done();
+        }
+      });
+    });
+    //
+    //it('should unable connect to mqtt server when username & password error', function (done) {
+    //  var client = mqtt.connect('http://127.0.0.1', {
+    //    username: 'root',
+    //    password: 'password'
+    //  });
+    //
+    //  client.on('connect', function (packet) {
+    //    console.log("================");
+    //    console.log(packet);
+    //    if(packet.returnCode === -1){
+    //      client.end();
+    //      done();
+    //    }
+    //  });
+    //});
+
+  });
+  it('should able to sub/pub message', function (done) {
     var client = mqtt.connect('http://127.0.0.1', {
       username: 'root',
       password: 'root'
     });
 
     client.on('connect', function () {
-      client.publish('root', 'coap');
-      client.subscribe('root');
-      client.unsubscribe('root');
-      client.end();
-      done();
+      client.subscribe('device/1');
+      var payload = '{"temperature": 3"}';
+        client.publish('device/1', payload);
+      client.on('message', function(topic, message) {
+        if((message.toString()) === payload){
+          client.end();
+          done();
+        }
+      });
     });
   });
 });
